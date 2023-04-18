edce34209c9f5dcfdf1d4d037f39158863af96e7
ekimeel <michaeljlee0@gmail.com>
2023-04-18 06:17:41 -0400

    first commit

---
+++ pkg/plugin/plugin.go
@@ -0,0 +1,31 @@
+package plugin
+
+import (
+	"github.com/ekimeel/sabal-pb/pb"
+	"time"
+)
+
+type Status int8
+
+const (
+	None Status = iota
+	Running
+	Ready
+	Error
+)
+
+type Plugin interface {
+	Run() error
+	Install(env *Environment) error
+	Name() string
+	Version() string
+	Config() Config
+	Status() int8
+	LastRuntime() time.Time
+}
+
+type Environment struct {
+	EquipService  pb.EquipServiceClient
+	PointService  pb.PointServiceClient
+	MetricService pb.MetricServiceClient
+}
