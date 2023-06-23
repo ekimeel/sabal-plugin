5fc07665295e1942c1a4b534ba0c4344e917702a
ekimeel <michaeljlee0@gmail.com>
2023-06-23 05:54:27 -0400

    added config

---
+++ pkg/plugin/config.go
@@ -0,0 +1,8 @@
+package plugin
+
+type Config struct {
+	Name        string                 `json:"name"`
+	Description string                 `json:"description"`
+	Version     string                 `json:"version"`
+	Properties  map[string]interface{} `json:"properties"`
+}
