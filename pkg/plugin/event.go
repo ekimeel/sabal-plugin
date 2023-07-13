7289bfa86859199402163a1c0faab8e5cd3d3dbf
ekimeel <michaeljlee0@gmail.com>
2023-07-13 05:39:33 -0400

    comments and remove error and add context

--- pkg/plugin/event.go
+++ pkg/plugin/event.go
@@ -2,6 +2,11 @@ package plugin
 
 import "github.com/ekimeel/sabal-pb/pb"
 
+// Event is a struct that represents an event that a Plugin can process.
+// It includes a slice of Metrics, where each Metric represents a single
+// data point or measurement at a certain point in time. The exact structure
+// and interpretation of a Metric is dependent on the specific application
+// and the specific plugin that processes the event.
 type Event struct {
 	Metrics []pb.Metric
 }
--- pkg/plugin/plugin.go
+++ pkg/plugin/plugin.go
@@ -2,15 +2,27 @@ package plugin
 
 import (
 	"context"
-	"plugin"
 )
 
+// Plugin is an interface for different kinds of plugins that can
+// process events, can be installed in an environment, and have a name.
 type Plugin interface {
+	// Process is a method that is supposed to process an Event in a certain way.
+	// The implementation of this method depends on the specific plugin.
+	// The context.Context argument can be used for cancellation, timeouts, and
+	// to pass other context-specific values.
 	Process(context context.Context, event Event)
-	Install(env *Environment) error
-	Name() string
-}
 
-type PluginManager interface {
-	RunChannel() chan plugin.Plugin
+	// Install is a method that is supposed to install the plugin in the given environment.
+	// The method receives an Environment object, which it can use to perform the installation.
+	// The exact steps for installation are dependent on the plugin and the environment.
+	// If the installation is successful, the method should return nil. Otherwise, it
+	// should return an error that describes what went wrong.
+	// The context.Context argument can be used for cancellation, timeouts, and
+	// to pass other context-specific values.
+	Install(context context.Context, env *Environment) error
+
+	// Name is a method that returns the name of the plugin. The name is expected to be
+	// unique among all plugins, and it can be used for logging, error reporting, etc.
+	Name() string
 }
